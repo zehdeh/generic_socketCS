be451046b2a8f0ceb4b06bf8c75d7b584fb78f18
Fabian Fritz <fabianfuture@web.de>
2015-02-24 23:32:03 -0600

    Scripted auto-commit on change (2015-02-24 23:32:03) by gitwatch.sh

---
+++ include/net/generic/ConnectionSettings.h
@@ -0,0 +1,53 @@
+#ifndef CONNECTIONSETTINGS_HEADER
+#define CONNECTIONSETTINGS_HEADER
+
+class ConnectionSettings {
+public:
+	void clientIO() {
+		std::string inputBuffer;
+		while(true) {
+			std::cout << "Please choose encryption or decryption: (Type e or d and press enter)" << std::endl;
+			getline(std::cin, inputBuffer);
+
+			mode = inputBuffer[0];
+			if(mode == 'e' || mode == 'd') {
+				break;
+			} else {
+				std::cout << "Input not recognized!" << std::endl;
+			}
+		}
+		std::cout << "Pick a cipher: (Type number and press enter)" << std::endl;
+		std::cout << "1. AES" << std::endl;
+		std::cout << "2. BF" << std::endl;
+		std::cout << "3. DES" << std::endl;
+
+		mode = 'e';
+		while(true) {
+			getline(std::cin, inputBuffer);
+			cipherNo = inputBuffer[0] - '0';
+			if(cipherNo >= 1 && cipherNo <= 3) {
+				break;
+			} else {
+				std::cout << "Input not recognized! Repeat:" << std::endl;
+			}
+		}
+
+		if(mode == 'e') {
+			std::cout << "Enter your plaintext" << std::endl;
+		} else {
+			std::cout << "Enter your encrypted text" << std::endl;
+		}
+
+		getline(std::cin,text);
+
+	}
+	char getMode() { return mode; }
+	unsigned int getCipherNo() { return cipherNo; }
+	std::string getText() { return text; }
+private:
+	char mode;
+	unsigned int cipherNo;
+	std::string text;
+};
+
+#endif
