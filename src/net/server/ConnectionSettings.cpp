33a657a1674dc457215cf95ad7da95393d4617ff
Fabian Fritz <fabianfuture@web.de>
2015-02-24 23:56:20 -0600

    Scripted auto-commit on change (2015-02-24 23:56:20) by gitwatch.sh

--- src/net/server/ConnectionSettings.cpp
+++ src/net/server/ConnectionSettings.cpp
@@ -1,9 +1,12 @@
 #include "net/generic/ConnectionSettings.h"
 #include "net/server/ServerSocket.h"
 
+#include <iostream>
+
 void ConnectionSettings::serverIO(ServerSocket& socket) {
 	std::string buffer;
 	socket >> buffer;
+	std::cout << buffer << std::endl;
 	mode = buffer[0];
 
 	socket >> buffer;
